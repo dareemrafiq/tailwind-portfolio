dd473c7afaceb9a974f711835d7269e9217b917e
dareemrafiq <dareemrafiq100@gmail.com>
2024-12-20 21:22:44 +0500

    first commit

---
+++ src/components/About.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div id="about" className="container pt-32">
+      <h2 className="text-4xl md:text-5xl" data-aos="zoom-in-up">About Me</h2>
+      <p className="text-gray-500 pt-4 text-3xl" data-aos="zoom-in-up">
+      I am a student at GIAIC,pursuing a course in Artificial Intelligence, Web 3.0, & Metaverse.
+      Passionate about technology and constantly learning new skills to stay up-to-date to the latest.
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="mt-16 py-6 text-center bg-yellow-600">
+      <div>&#169; Dareem| All rights reserved</div>
+      
+    </div>
+  )
+}
+
+export default Footer
